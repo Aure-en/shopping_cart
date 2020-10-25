a8ac0585342f7daa122bb85fe1a0f1a8265997ea
Aureen <yukiansff@gmail.com>
2020-10-25 16:02:20 +0100

    Styled ShopDetails

--- src/components/ShopDetails.js
+++ src/components/ShopDetails.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import menu from '../data/menu'
+import styles_menu from '../styles/layout/menu.module.scss'
+import styles from '../styles/layout/shopDetails.module.scss'
 
 function ShopDetails( {cart, updateCart, match} ) {
 
@@ -35,28 +37,50 @@ function ShopDetails( {cart, updateCart, match} ) {
   }, [match.params.id])
 
   const changeQuantity = (event) => {
-    setQuantity(event.target.value)
+    setQuantity(+event.target.textContent)
+  }
+
+  const incrementQuantity = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1)
+  }
+
+  const decrementQuantity = () => {
+    if (quantity < 1) return
+    setQuantity((prevQuantity) => prevQuantity - 1)
+  }
+
+  const validateQuantity = () => {
+    if (quantity < 0) {
+      setQuantity(0)
+    }
   }
 
   return (
-    <main>
-      <img src={image.src} alt={image.alt} />
-      <div className="menu__name">{name}</div>
-      <div className="menu__description">{description}</div>
-      <div className="menu__price">{price}</div>
-      <div className="menu__details">
-        {
-          details.map((course, index) => (
-          <li key={index}>
-            <div>{course.name}</div>
-            <div>{course.description}</div>
-          </li>
-          ))
-        }
+    <main className={styles.shopDetails}>
+      <img className={styles.background} src={image.src} alt={image.alt} />
+      <div className={`${styles.content} box`}>
+        <h1 className={styles.menu__title}>{name}</h1>
+        <span className="decoration">⬧</span>
+        <div className={styles.description}>{description}</div>
+        <ul className={styles.details}>
+          {
+            details.map((course, index) => (
+            <li className={styles.course} key={index}>
+              <div className={styles.course__name}>{course.name}</div>
+              <div>{course.description}</div>
+            </li>
+            ))
+          }
+        </ul>
+        <div className={styles_menu.menu__price}>{price} per person</div>
+        <div className={styles.menu__order}>
+        Order for
+        <button className="btn" type="button" onClick={decrementQuantity}>-</button>
+        <span className={styles.menu__input} onInput={changeQuantity} onBlur={validateQuantity} contentEditable suppressContentEditableWarning={true}>{quantity}</span>
+        <button className="btn" type="button" onClick={incrementQuantity}>+</button>
       </div>
-      <div className="menu__order">
-        Order for <input type="number" minvalue="0" value={quantity} onChange={changeQuantity}/>
-        <button type="button" onClick={() => addToCart({id, quantity})}>+</button>
+
+      <button className={`${styles_menu.menu__submit} btn--big`} type="button" onClick={() => addToCart({ id, quantity })}>+</button>
       </div>
     </main>
   )
